9b4a6c250a6ce4d1543cd89998d74c80274201d4
Patryk Stefanowicz <patrykstefanowicz95@gmail.com>
2020-12-08 20:52:06 +0100

    new week-summary and padding header

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ console.log(firstName);
 console.log(age);
 
 console.log(`Siema, nazywam się ${firstName} oraz mam ${age} lat.`);
+
+const heading = document.querySelector('.main__heading--js')
+
+console.log(heading);
+
+heading.innerHTML = `Coś tam się udało za pomocą querySelectora`
+
+const emptyParagraph = document.querySelector('.about__description--js');
+
+emptyParagraph.innerHTML = `dopisane za pomocą JavaScriptu`;
