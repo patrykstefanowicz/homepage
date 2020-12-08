b8f10bd89faa8cf5a3edc60d732992d1b11ce917
Patryk Stefanowicz <patrykstefanowicz95@gmail.com>
2020-12-08 21:08:39 +0100

    calculate function js

--- main.js
+++ main.js
@@ -6,12 +6,21 @@ console.log(age);
 
 console.log(`Siema, nazywam się ${firstName} oraz mam ${age} lat.`);
 
-const heading = document.querySelector('.main__heading--js')
+const heading = document.querySelector(".main__heading--js");
 
 console.log(heading);
 
-heading.innerHTML = `Coś tam się udało za pomocą querySelectora`
+heading.innerHTML = `Coś tam się udało za pomocą querySelectora`;
 
-const emptyParagraph = document.querySelector('.about__description--js');
+const emptyParagraph = document.querySelector(".about__description--js");
 
 emptyParagraph.innerHTML = `dopisane za pomocą JavaScriptu`;
+
+function calculate(myNumber) {
+  console.log(`Dostałem ${myNumber}`);
+  return myNumber * 7;
+}
+
+const myResult = calculate(2);
+
+console.log(myResult);
