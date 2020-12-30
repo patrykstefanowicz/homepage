fb9a1549d563126789802fe70fad5947a367a348
Patryk Stefanowicz <patrykstefanowicz95@gmail.com>
2020-12-30 14:42:13 +0100

    hamburger menu

--- main.js
+++ main.js
@@ -128,3 +128,12 @@ const myClick = () => {
 }
 
 button.addEventListener('click', myClick);
+
+
+  const hamburger = document.querySelector('.hamburger--js');
+  hamburger.addEventListener('click',() =>{
+  const nav = document.querySelector('.navigation--js');
+    nav.classList.toggle('navigation--open');
+  });
+
+  
